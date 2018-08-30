87dc71118c045c8827b9618472504a8bb793cb83
huanliu78 <huanliu78@creditease.cn>
2018-08-30 16:02:52 +0800

    冒泡排序

---
+++ python37_project/算法/bubble_sort.py
@@ -0,0 +1,22 @@
+# 冒泡排序
+def bubble_sort(lists):
+    for i in range(len(lists) - 1):
+        print(i)
+        for j in range(len(lists) - 1 - i):
+            if lists[j] > lists[j + 1]:
+                print(lists)
+                lists[j], lists[j + 1] = lists[j + 1], lists[j]
+    return lists
+# print('--------')
+# print('错误示范')
+# def bubble_sortt(lists):
+print(bubble_sort([2,1,3,0]))
+#     count = len(lists)
+#     for i in range(0, count):
+#         print(i)
+#         for j in range(i + 1, count):
+#             if lists[i] > lists[j]:
+#                 print(lists)
+#                 lists[i], lists[j] = lists[j], lists[i]
+#     return lists
+# print(bubble_sortt([2,1,3,0]))
